dc36a8be948e21ba2ee8c1abf1b4a85990ab177e
Jan Luca Scheerer <scheerer@cs.tum.edu>
2023-11-23 20:23:46 +0100

    Pass output filename as parameter

--- wd_migrate.cc
+++ wd_migrate.cc
@@ -9,17 +9,16 @@
 
 auto main(int argc, char **argv) -> int {
   using namespace wd_migrate;
-  if (argc <= 1) {
-    std::cout << "usage: " << argv[0] << " <filename>" << std::endl;
+  if (argc <= 2) {
+    std::cout << "usage: " << argv[0] << " <filename> <output>" << std::endl;
     return -1;
   }
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
   constexpr stats_handler_options options{.print_illegal_values = false};
-  auto handler =
-      stacked_handler(stats_handler<options>(), quantity_scale_handler(),
-                      csv_handler("data/example.csv"));
+  auto handler = stacked_handler(
+      stats_handler<options>(), quantity_scale_handler(), csv_handler(argv[2]));
   qualifiers_parser<decltype(handler)> parser;
   parser.parse(argv[1], &handler);
 
